f448b5176fcf97bdb9c674d44a88e88a55b3ee21
jurjees23 <s.jurjeesahamed555@gmail.com>
2022-09-29 11:46:18 +0400

    Updates

---
+++ precache-manifest.853120fa80d14ae43c98163a26ffd83c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54d13fad60b5bc9b635984d631d22130",
+    "url": "./index.html"
+  },
+  {
+    "revision": "eef9d2445607ab6f1dc6",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "d2e600d4e613a8cc9d4c",
+    "url": "./static/js/2.dd78984b.chunk.js"
+  },
+  {
+    "revision": "282561c935084219ab0369ef56f98a98",
+    "url": "./static/js/2.dd78984b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef9d2445607ab6f1dc6",
+    "url": "./static/js/main.f141d7b2.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.f141d7b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dfb7d38049655522100",
+    "url": "./static/js/runtime-main.f83a8c00.js"
+  }
+]);
