27227d36828cbfdb7545c763e456487d0b79ac70
jurjees23 <s.jurjeesahamed555@gmail.com>
2022-09-29 10:53:53 +0400

    initial commit

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,70 @@
-import React from 'react'
-
-import { ExampleComponent } from 'xdcpay-web3modal'
-import 'xdcpay-web3modal/dist/index.css'
+import React, { useState } from 'react';
+import Web3Modal from 'web3modal';
+import WalletConnect from "@walletconnect/web3-provider";
+import { ethers } from 'ethers';
+import { getXdcModal } from 'xdcpay-web3modal'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  const [provider, setProvider] = useState(null)
+  const [address, setAddress] = useState(null)
+
+  const web3Modal = new Web3Modal({
+    cacheProvider: true,
+    disableInjectedProvider: false,
+    providerOptions: {
+      walletconnect: {
+        package: WalletConnect, // required
+        options: {
+          infuraId: "223f20f418c34a758240a7f416435110", // Required
+          network: "mainnet",
+          qrcodeModalOptions: {
+            mobileLinks: ["rainbow", "metamask", "argent", "trust", "imtoken", "pillar"]
+          }
+        }
+      },
+      'custom-xdc': getXdcModal, // Add One line in  web3modal provider
+    }
+  });
+
+  const onConnect = async () => {
+
+    try {
+      const instance = await web3Modal.connect();
+      const providerConnect = new ethers.providers.Web3Provider(instance);
+      setProvider(providerConnect)
+
+    } catch (err) {
+      console.log("err", err)
+    }
+  }
+
+  const sendXdc = async () => {
+    const signer = provider.getSigner();
+    const address = await signer.getAddress();
+    setAddress(address)
+    console.log("Address", address)
+    // Acccounts now exposed
+    const params = [{
+      from: address,
+      to: "0x117c691d76c1d9c68e3709a87f7d496097f2b56f",
+      value: ethers.utils.parseUnits('1', 'ether').toHexString()
+    }];
+
+    const transactionHash = await provider.send('eth_sendTransaction', params)
+    console.log('transactionHash is ' + transactionHash);
+  }
+  return (
+    <div align="center">
+      <h3>How to connect XDC Pay wallet (chrome extension) with  Web3Modal</h3>
+
+
+
+      {!provider && <button type='button' onClick={onConnect}>Connect</button>}
+      {provider && <button type='button' onClick={sendXdc}>Send XDC</button>}
+
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
-import styles from './styles.module.css'
+import detectEthereumProvider from '@metamask/detect-provider'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+detectEthereumProvider().then(res => {
+  if (!res) {
+    throw new Error('Unable to detect window.ethereum.')
+  }
+})
+
+export const getXdcModal = {
+  display: {
+    name: 'XDC Pay',
+    logo: 'https://s2.coinmarketcap.com/static/img/coins/64x64/2634.png',
+    description: 'Connect with XDC Pay'
+  },
+  package: detectEthereumProvider,
+  connector: async (_detectEthereumProvider) => {
+    const provider = await _detectEthereumProvider();
+    await provider.enable();
+    return provider;
+  }
 }
